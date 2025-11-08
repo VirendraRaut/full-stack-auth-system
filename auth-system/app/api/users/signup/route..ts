4b6eda962963317e4fa59c90471490bb3f736ee4
VirendraRaut <virendraraut1306@gmail.com>
2025-11-08 19:22:21 +0530

    commit

--- auth-system/app/api/users/signup/route..ts
+++ auth-system/app/api/users/signup/route..ts
@@ -10,6 +10,9 @@ export async function POST(request: NextRequest) {
     const reqBody = await request.json();
     const { username, email, password } = reqBody;
     console.log(reqBody);
+
+    // check if user exists or not
+    const user = await User.findOne({ email });
   } catch (error: any) {
     return NextResponse.json({ error: error.message }, { status: 500 });
   }
