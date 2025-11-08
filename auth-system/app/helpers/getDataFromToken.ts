1fbab2db0885e885ed54d0114307fe6599501e32
VirendraRaut <virendraraut1306@gmail.com>
2025-11-08 21:36:58 +0530

    commit

---
+++ auth-system/app/helpers/getDataFromToken.ts
@@ -0,0 +1,12 @@
+import jwt from "jsonwebtoken";
+import { NextRequest } from "next/server";
+
+export const getDataFromToken = async (request: NextRequest) => {
+    try {
+        const token = request.cookies.get("token")?.value || "";
+        const decoded = jwt.verify(token, process.env.TOKEN_SECRET!);
+        return decoded;
+    } catch (error: any) {
+        throw new Error(error.message);
+    }
+}
