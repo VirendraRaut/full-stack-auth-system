5e8efc64b630c224237637bc5979211d64eb9778
VirendraRaut <virendraraut1306@gmail.com>
2025-11-08 21:39:21 +0530

    commit

--- auth-system/app/helpers/getDataFromToken.ts
+++ auth-system/app/helpers/getDataFromToken.ts
@@ -4,8 +4,8 @@ import { NextRequest } from "next/server";
 export const getDataFromToken = async (request: NextRequest) => {
     try {
         const token = request.cookies.get("token")?.value || "";
-        const decoded = jwt.verify(token, process.env.TOKEN_SECRET!);
-        return decoded;
+        const decoded:any = jwt.verify(token, process.env.TOKEN_SECRET!);
+        return decoded.id;
     } catch (error: any) {
         throw new Error(error.message);
     }
