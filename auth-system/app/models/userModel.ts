94c081cac5ae6003e1af1973f5533c6d1fe5b1f1
VirendraRaut <virendraraut1306@gmail.com>
2025-11-08 18:58:36 +0530

    export default user

--- auth-system/app/models/userModel.ts
+++ auth-system/app/models/userModel.ts
@@ -4,4 +4,5 @@ const userSchema = new mongoose.Schema({
 
 })
 
-export const User = mongoose.models.User || mongoose.model("User", userSchema);
+const User = mongoose.models.User || mongoose.model("User", userSchema);
+export default User;
