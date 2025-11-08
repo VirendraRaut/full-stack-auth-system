d6a6a69d9c26d3a79ddd0ad8495880671712c979
VirendraRaut <virendraraut1306@gmail.com>
2025-11-08 20:55:45 +0530

    logout controller and route

--- auth-system/app/api/users/logout/route.ts
+++ auth-system/app/api/users/logout/route.ts
@@ -5,3 +5,12 @@ import jwt from "jsonwebtoken";
 import { NextRequest, NextResponse } from "next/server";
 
 connectDB();
+
+export async function GET(request:NextRequest) {
+    try {
+        
+    } catch (error:any) {
+        return NextResponse.json({ error: error.message }, { status: 500 });
+
+    }
+}
