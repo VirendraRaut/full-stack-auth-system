f94f946a5ac72c6db2174a76846ef73f97339b22
VirendraRaut <virendraraut1306@gmail.com>
2025-11-08 19:18:08 +0530

    commit

--- auth-system/app/api/users/signup/route..ts
+++ auth-system/app/api/users/signup/route..ts
@@ -1,2 +1,6 @@
 import { connectDB } from "@/app/db/db";
 import User from "@/app/models/userModel";
+import bcrypt from "bcryptjs";
+import { NextRequest, NextResponse } from "next/server";
+
+connectDB()
