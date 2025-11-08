61c076e04523b5fdb2198af1a135d4d67ffae529
VirendraRaut <virendraraut1306@gmail.com>
2025-11-08 19:19:53 +0530

    commit

--- auth-system/app/api/users/signup/route..ts
+++ auth-system/app/api/users/signup/route..ts
@@ -4,3 +4,13 @@ import bcryptjs from "bcryptjs";
 import { NextRequest, NextResponse } from "next/server";
 
 connectDB();
+
+export async function POST(request: NextRequest) {
+  try {
+    const reqBody = await request.json();
+    const { username, email, password } = reqBody;
+    console.log(reqBody);
+  } catch (error: any) {
+    return NextResponse.json({ error: error.message }, { status: 500 });
+  }
+}
