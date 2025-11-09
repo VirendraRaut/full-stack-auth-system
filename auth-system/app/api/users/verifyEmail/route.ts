6b5e3173eba720b463011a56c1f6517e626a6893
VirendraRaut <virendraraut1306@gmail.com>
2025-11-09 12:57:14 +0530

    api create for verify email

--- auth-system/app/api/users/verifyEmail/route.ts
+++ auth-system/app/api/users/verifyEmail/route.ts
@@ -1,4 +1,13 @@
 import { connectDB } from "@/app/db/db";
 import { NextRequest, NextResponse } from "next/server";
+import User from "@/app/models/userModel";
 
 connectDB();
+
+export async function POST(request: NextRequest) {
+    try {
+        
+    } catch (error: any) {
+        return NextResponse.json({ error: error.message }, { status: 500 });
+    }
+}
